b221904ef61f8fa0a284a1c32d7cfa978e4f44d8
Bo Chean <bchean@gmail.com>
2015-11-13 00:26:29 -0800

    Fix async gulp task issue by merging streams.

--- chrome-extension/gulpfile.js
+++ chrome-extension/gulpfile.js
@@ -1,6 +1,7 @@
 var child_process = require('child_process'),
     del = require('del'),
-    gulp = require('gulp');
+    gulp = require('gulp'),
+    mergeStreams = require('merge-stream');
 
 var BUILD_DIR = 'build';
 var EXT_NAME = 'ytb-client';
@@ -15,17 +16,19 @@ gulp.task('clean', function() {
 
 gulp.task('copy', ['clean'], function() {
     // Would be nice if we could print the files being copied here.
-    gulp.src('img/**')
+    var copyImg = gulp.src('img/**')
         .pipe(gulp.dest(BUILD_DIR + '/img'));
-    gulp.src('js/**')
+    var copyJs = gulp.src('js/**')
         .pipe(gulp.dest(BUILD_DIR + '/js'));
-    return gulp.src('manifest.json')
+    var copyManifest = gulp.src('manifest.json')
         .pipe(gulp.dest(BUILD_DIR));
+    return mergeStreams(copyImg, copyJs, copyManifest);
 });
 
 gulp.task('crx', ['copy'], function() {
     // Not very gulp-ey... Wonder how we can refactor this.
     var crxCommand = 'bash crxmake.sh ' + BUILD_DIR + ' key.pem ' + EXT_NAME;
+    console.log('$ ' + crxCommand);
     console.log(child_process.execSync(crxCommand).toString().trim());
 });
 
