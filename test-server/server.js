08e39a69a97faa2bb41875b74ae79d88581ca058
Bo Chean <bchean@gmail.com>
2016-01-08 01:32:58 -0800

    Test server tweaks.

--- test-server/server.js
+++ test-server/server.js
@@ -3,5 +3,5 @@ var app = express();
 
 app.use(express.static('public'));
 app.listen(5001, function() {
-    console.log('Test app started.');
+    console.log('Test app started on port ' + 5001);
 });
